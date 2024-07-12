89be352b4c19342968747e4bc4b85a971d21a0fe
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-07-12 20:33:47 +0000

    final setup

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,11 @@
-import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib/framework' // Make sure this import is correct
+import Vuetify from 'vuetify/lib/framework'
+import '@mdi/font/css/materialdesignicons.css' // Make sure this path is correct
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi' // default - only for display purposes
+    iconfont: 'mdi' // This sets the default icon font to Material Design Icons
   }
 })
