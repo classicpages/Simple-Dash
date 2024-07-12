bb508c7c17369562e781ec9aea1cbcf239a5480f
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-07-12 12:17:35 +0000

    main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
